975b044f4269761e12d726c86f807b0812f6313e
Krish Devkota <chrisdevkota77@gmail.com>
2022-07-06 11:10:47 +0545

    Console Play success

--- app.js
+++ app.js
@@ -1,80 +1,70 @@
-// each 3 btns event listener to game 
-//comp play - returns any one.
-// function playround with player Selection and comp value
-    // scorecard 
-// verdict function
-    //once scores reach 5; reset function
-// reset btn event listener; if so clear scores and go to game
-//
-//
-//
-const res = document.querySelector('#roundResults');
-const btn = document.querySelectorAll('button');
-const comp= document.querySelector('#computerScore');
-const ply= document.querySelector('#playerScore');
-const reset = document.querySelector('#reset');
+//RPS game , console play first. Then move to DOM 
+//introduce two variables storing string values ie playerSelection and computerSelection
+//run prompt to get input for "playerSelection" which shuold be case-insensitive.
+//for computerSelection, call function ComputerPlay that randomly returns an option
+//create function playRound with two parameters playerSelection and computerSelection 
+//inside playRound, create a conditional checking the precedence of each option over the others. 
+//console.log playRound.
+//return string result (i.e Win, lose or draw) from conditional specifying to the user specifying the precedence process.  
+//introduce integer variables "compCount" and "playerCount" with intValue 0. increment by one every time one of the two wins a game. 
+//create function "game" that calls "playRound" function for 5 rounds with "compCount" and playerCount being incremented as per the precendence.
+//create conditional comparing compCount and playerCount and return game result accordingly. [Console Play complete]
+//[Integrating DOM] 
 
 
-reset.addEventListener('click',()=>{
-    window.location.reload();
-});
+let playerSelection;
+let computerSelection;
+let compCount = 0 ; let playerCount =0;
+game();
 
+function computerPlay () {
+    let optSelect = Math.floor(Math.random()*3);
+    console.log(optSelect);
+    let result;
 
-
-btn.forEach(button =>{
-     button.addEventListener('click',getValue)
-
-});
-
-let compScore=0;
-let playerScore=0;
-let playerChoice;
-
-    function computerPlay(){
-            let opt= ['Rock','Paper','Scissors'];
-            return opt[Math.floor(Math.random()*3)];      
-        }
-        console.log(computerPlay()); //check 1 
-
-        function playerRound(playerSelection,computerSelection){
-            if (playerSelection===computerSelection){
-                comp.textContent= ++compScore;
-                ply.textContent=  ++playerScore;
-            }else if (((playerSelection=="Rock") && (computerSelection=="Scissors"))||
-                      ((playerSelection=="Paper") && (computerSelection=="Rock")) ||
-                      ((playerSelection=="Scissors") && (computerSelection=="Paper"))){
-                        ply.textContent= ++playerScore;
-            }else {comp.textContent= ++compScore};
-            console.log(compScore,playerScore);
-            
-            if((compScore==5)||(playerScore==5)){
-                verdict();
-            }
-        }
-        
-    
-
-// console.log(compScore,playerScore);         //check 2
-function verdict(){
-    if (compScore<playerScore){
-   res.textContent = "You win!";
-    } else if(playerScore<compScore){
-     res.textContent= "You Lose!";
-    }else res.textContent= "It's a tie";
-    console.log('hello');
-
-    btn.forEach(button=>{
-        button.removeEventListener('click',getValue);
-    })
-    
+    if (optSelect ==1){
+        result = "rock";
+    }else if (optSelect==2){
+        result = "paper"
+    }else {result = "scissors"}
+    console.log(result);
+    return result;    
 }
 
-function getValue(e){
-    let playerSelection = e.target.id;
-    playerChoice= e.target.textContent;
-    playerRound(playerSelection,computerPlay());
+function playRound(playerSelection,computerSelection){
+        if (playerSelection===computerSelection){
+            console.log ("It's a draw!");
+        }else if ((playerSelection === "rock" && computerSelection === "scissors" )||( playerSelection === "scissors" && computerSelection === "paper") ||(playerSelection === "paper" && computerSelection === "rock")){
+            console.log("W!");
+            playerCount +=1;
+        }else {
+            console.log("L");
+            compCount += 1;
+        }
 }
 
+function final_verdict(){
+    let verdict;
+
+    if (compCount>playerCount){
+        verdict ="You lost the game!";
+    }else if (playerCount>compCount){
+        verdict= "You win the game!";
+    }else {
+        verdict= "DRAW";
+    }
+    return verdict;  
+}
+
+function game () {
+    for (let i=0 ;i<=5;i++){
+        playerSelection = prompt ("Enter an option").toLowerCase();
+        computerSelection = computerPlay();
+        console.log(playRound(playerSelection,computerSelection));
+        console.log(playerCount,compCount);
+    }
+    console.log(final_verdict());
+}
 
 
 
